2964e16c55d413a5f69c99a48cff773f8a62879f
Paul Kuruvilla <rohitpaulk@gmail.com>
2025-08-13 16:54:01 -0700

    Implement file operations with timing: copy, hardlink, and symlink functions.

--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	os.RemoveAll("/tmp/test-1.db")
+	os.RemoveAll("/tmp/test-2.db")
+	os.RemoveAll("/tmp/test-3.db")
+
+	measureTime("cp", "./companies.db", "/tmp/test-1.db", copyFile)
+	measureTime("hardlink", "./companies.db", "/tmp/test-2.db", hardLinkFile)
+	measureTime("symlink", "./companies.db", "/tmp/test-3.db", symLinkFile)
+}
+
+func measureTime(operation, src, dst string, fn func(string, string) error) {
+	start := time.Now()
+	fmt.Printf("Starting %s\n", operation)
+
+	if err := fn(src, dst); err != nil {
+		fmt.Printf("- %s failed: %v\n", operation, err)
+	} else {
+		fmt.Printf("- %v for %s\n", time.Since(start), operation)
+	}
+}
+
+func copyFile(src, dst string) error {
+	cmd := exec.Command("cp", src, dst)
+	return cmd.Run()
+}
+
+func hardLinkFile(src, dst string) error {
+	return os.Link(src, dst)
+}
+
+func symLinkFile(src, dst string) error {
+	return os.Symlink(src, dst)
 }
