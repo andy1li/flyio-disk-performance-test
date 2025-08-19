de8eda4d64e3ee3e78f067199dcf9840cbc98fa5
Andy Li <1450947+andy1li@users.noreply.github.com>
2025-08-20 04:40:45 +0800

    Enhance main.go to improve database query timing logs by specifying the database paths in the measurement labels for better clarity.

--- main.go
+++ main.go
@@ -25,7 +25,8 @@ func main() {
 	fmt.Print(string(output))
 
 	measureTime("symlink", "/var/opt/tester/companies.db", "/app/test-1.db", symLinkFile)
-	measureTime("db.Query", "./test-1.db", "SELECT id, name FROM companies WHERE country = 'micronesia'", dbQuery)
+	measureTime("db.Query (/var/opt/tester/companies.db)", "./test-1.db", "SELECT id, name FROM companies WHERE country = 'micronesia'", dbQuery)
+	measureTime("db.Query (/app/test-1.db)", "./test-1.db", "SELECT id, name FROM companies WHERE country = 'micronesia'", dbQuery)
 
 	measureTime("cp", "/var/opt/tester/companies.db", "/app/test-3.db", copyFile)
 	measureTime("hardlink", "/var/opt/tester/companies.db", "/app/test-2.db", hardLinkFile)
