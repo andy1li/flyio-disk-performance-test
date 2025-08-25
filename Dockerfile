6bfa6f27ca017ee03f7ad0f02665343cbff8543f
Andy Li <1450947+andy1li@users.noreply.github.com>
2025-08-26 06:05:43 +0800

    Update Dockerfile to remove test-1.db and change CMD to use strace for linking companies.db to test-1.db. Modify main.go to add timing measurement for a new SQL query, enhancing performance evaluation.

--- Dockerfile
+++ Dockerfile
@@ -10,4 +10,6 @@ RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o main .
 RUN chmod +x main
 
 WORKDIR /app
-CMD ["/var/opt/tester/main"]
+# CMD ["/var/opt/tester/main"]
+RUN rm "/app/test-1.db"
+CMD ["strace", "ln", "/var/opt/tester/companies.db", "/app/test-1.db"]
--- main.go
+++ main.go
@@ -35,10 +35,10 @@ func main() {
 	// 	i *= 2
 	// }
 
+	measureTime("realSqlite limit 1", "./test-1.db", "SELECT id, name FROM companies LIMIT 1", realSqlite)
 	// measureTime("realSqlite", "./test-1.db", "SELECT id, name FROM companies WHERE country = 'micronesia'", realSqlite)
 	// measureTime("realSqlite again", "./test-1.db", "SELECT id, name FROM companies WHERE country = 'micronesia'", realSqlite)
 
-	measureTime("realSqlite limit 1", "./test-1.db", "SELECT id, name FROM companies LIMIT 1", realSqlite)
 	measureTime("db.Query limit 1", "./test-1.db", "SELECT id, name FROM companies LIMIT 1", dbQueryExplain)
 	measureTime("db.Query explain", "./test-1.db", "SELECT id, name FROM companies WHERE country = 'micronesia'", dbQueryExplain)
 	// measureTime("db.Query (/var/opt/tester/companies.db)", "/var/opt/tester/companies.db", "SELECT id, name FROM companies WHERE country = 'micronesia'", dbQuery)
